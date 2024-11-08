1ecf54570a600fc7761becf7eaaaeb0914d1551e
ostmannen <60339614+ostmannen@users.noreply.github.com>
2024-11-08 19:32:21 +0100

    idk man pls god dont kill me

--- Assets/Scripts/PickManager.cs
+++ Assets/Scripts/PickManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PickManager : MonoBehaviour
 {
-    private Camera MainCamera;
+    [SerializeField] private Camera MainCamera;
     [NonSerialized] public List<GameObject> pickedCharacters = new List<GameObject>();
     void Start()
     {
@@ -13,15 +13,39 @@ public class PickManager : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)){
+        if (Input.GetMouseButtonDown(0))
+        {
             TryPick();
         }
     }
-    public void AddCharacters(GameObject character){
+    public void AddCharacters(GameObject character)
+    {
         pickedCharacters.Add(character);
     }
-    private void TryPick(){
+    private void TryPick()
+    {
+        Debug.Log("idk");
         RaycastHit hit;
-        //if (Physics.Raycast(MainCamera.ScreenToWorldPoint(Input.mousePosition)))
+        if (Physics.Raycast(MainCamera.ScreenToWorldPoint(Input.mousePosition), MainCamera.transform.forward, out hit, 1000f))
+        {
+            Debug.Log("pppp");
+            Debug.Log(hit);
+            GameObject pickedObject = hit.collider.gameObject;
+            if (pickedObject.tag == "Pickable")
+            {
+                pickedCharacters.Add(pickedObject);
+                Destroy(pickedObject);
+            }
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (MainCamera != null)
+        {
+            // Draws a blue line from this transform to the target
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(MainCamera.ScreenToWorldPoint(Input.mousePosition), MainCamera.transform.forward);
+            }
+        
     }
 }
