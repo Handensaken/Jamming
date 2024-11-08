0b18ebaf635be481b6c8142ace6f6db6345663e4
ostmannen <60339614+ostmannen@users.noreply.github.com>
2024-11-08 23:18:28 +0100

    hover and prefab changes

--- Assets/Scripts/StrenthOrSomeShit.cs
+++ Assets/Scripts/StrenthOrSomeShit.cs
@@ -9,14 +9,26 @@ public class StrenthOrSomeShit : MonoBehaviour
     public GameObject canvasPrefab;
     [Range(1,100)]
     public int strength = 1;
+    private bool Hover = false;
     
     void Start(){
         gameObject.tag = "Pickable";
         pickManager = FindAnyObjectByType<PickManager>();
     }
+    void Update(){
+        if (Hover){
+            
+        }
+    }
     public void Selected(){
         transform.parent = null;
         transform.position = Vector3.zero;
         GameEventsManager.instance.pickedEvents.Picked(this.gameObject);
+    }
+    public void OnHoverEnter(){
+        Hover = true;
+    }
+    public void OnHoverExit(){
+        Hover = false;
     }
 }
