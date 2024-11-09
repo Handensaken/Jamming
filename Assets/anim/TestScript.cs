ffb9aed7ed76458e7709807b8c2970aabbed9017
Handensaken <maxmax.larsson@gmail.com>
2024-11-09 13:27:30 +0100

    Set up animation behaviour

--- Assets/anim/TestScript.cs
+++ Assets/anim/TestScript.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestScript : MonoBehaviour
-{   
+{
     private Animator Huhrensohn;
+
     // Start is called before the first frame update
     void Start()
     {
         float offset = Random.Range(0, 0.9f);
-        float speedmult = Random.Range(1,1.5f);
+        float speedmult = Random.Range(1, 1.5f);
         Huhrensohn = GetComponent<Animator>();
         Huhrensohn.SetFloat("Offset", offset);
         Huhrensohn.SetFloat("SpeedMult", speedmult);
@@ -18,6 +19,26 @@ public class TestScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Random.Range(0, 10) > 8)
+        {
+            SetRandomFloat("AltSadVal");
+            SetRandomFloat("HappyAltVal");
+        }
+    }
+
+    public void SetTrigger(string name)
+    {
+        Huhrensohn.SetTrigger(name);
+    }
+
+    public void SetBool(string name, bool value)
+    {
+        Huhrensohn.SetBool(name, value);
+    }
+
+    public void SetRandomFloat(string name)
+    {
+        float r = Random.Range(0f, 2f);
+        Huhrensohn.SetFloat(name, r);
     }
 }
