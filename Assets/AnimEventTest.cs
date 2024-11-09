7efc75c4aa927ca633e2ce12a193739b28af8c0f
Handensaken <maxmax.larsson@gmail.com>
2024-11-09 17:05:19 +0100

    I do not fucking know ingenting tror jag?????

--- Assets/AnimEventTest.cs
+++ Assets/AnimEventTest.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class AnimEventTest : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void cunt(){
-        
-    }
+    void Update() { }
+
+    public void cunt() { }
 }
